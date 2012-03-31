ebafb955028a5e852fe9c2a201eb28ec2f05a6be
Ian <typppo@gmail.com>
2012-03-31 15:46:55 -0700

    adding js handling, server call

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+$(function() {
+
+  $('#submit').on('click', function() {
+    var email = $('#email').val();
+    if ($.trim(email) == '')
+      return false;
+
+
+    $.post('/signup', {'email': email}, function(data) {
+      if (data.success) {
+        $('#success-body').text('Thanks, ' + email + '! You\'ll start getting emails tomorrow morning.');
+        $('#success-modal').modal();
+        $('#email').val('');
+      }
+      else {
+        alert("Something went wrong and we couldn't add you, sorry :(");
+      }
+    }).error(function() {
+      alert("Something went wrong and we couldn't add you, sorry :(");
+    });
+
+
+    return false;
+  });
+});
